ed8b66e2a47a283104e141cae81fde983469bba4
thanhliem26 <75157242+thanhliem26@users.noreply.github.com>
2020-12-03 09:59:29 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML/CSS/JS Tutorial</title>
+</head>
+<body>
+	<img src="https://i.pinimg.com/originals/3a/3c/ab/3a3cabd94347cf64dade52882308c780.jpg" alt="" />
+	<h1> Họ và tên: Phạm Văn Liêm</h1>
+	<h2> Age: 17</h2>
+	<a>Address: La Khê - Tố Hữu - Hà Đông</a>
+	<h3>Telephone:0866759002</h3>
+	<a href="http://lienquan.vn.com" target="_blank">My favorite game: Liên Quân Mobile</a>
+	<a>Addeess email: phamvanliem26122002@gmail.com</a>
+	<img src="https://i.imgur.com/1vRS8ZF.png" alt="image" />
+</body>
+</html>
